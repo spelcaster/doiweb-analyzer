b5aef04778d69e4edd42a99a871f74d4cc2b1bd5
Hugo Augusto Freitas do Carmo <hugrito.kun@gmail.com>
2019-10-28 16:52:05 -0300

    add resource for DOICodeTipoTransacao
    - Add migrations, model and seeder for DOICodeTipoTransacao;

--- src/Database/Seeds/DatabaseSeeder.php
+++ src/Database/Seeds/DatabaseSeeder.php
@@ -13,5 +13,7 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
+        $this->call(DOICodeTipoTransacaoSeeder::class);
+        $this->command->info('DOICodeTipoTransacaoSeeder loaded!');
     }
 }
