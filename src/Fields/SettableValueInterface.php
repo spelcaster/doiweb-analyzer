7053be1949093d51caab9ee56a6ca39c09db9e31
Hugo Augusto Freitas do Carmo <hugrito.kun@gmail.com>
2019-11-05 20:24:28 -0300

    add SettableValueInterface
    - The interface SettableValueInterface forces the implementation of the
      method setValue in Field classes;

---
+++ src/Fields/SettableValueInterface.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace DOIWeb\Fields;
+
+/**
+ * Interface SettableValueInterface
+ * @author yourname
+ */
+interface SettableValueInterface
+{
+    public function setValue($value);
+}
